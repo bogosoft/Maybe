5b9e5e924e35e4bb6bd97f22b7ed4b62b973317f
Clayton Roth <croth0627@yahoo.com>
2017-10-29 23:55:46 -0700

    forcing a build

--- Bogosoft.Maybe/Properties/AssemblyInfo.cs
+++ Bogosoft.Maybe/Properties/AssemblyInfo.cs
@@ -18,7 +18,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f825321b-88eb-475d-9edc-266bfe61be80")]
 
 // Version information for an assembly consists of the following four values:
